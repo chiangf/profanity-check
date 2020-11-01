2b056fe9f49f6c74d4577060ad6d29c87617a704
Frank Chiang <chiangf@gmail.com>
2020-11-01 14:55:14 -0500

    Add trainer and update requirements

--- profanity_check/profanity_check.py
+++ profanity_check/profanity_check.py
@@ -2,14 +2,23 @@ import pkg_resources
 import numpy as np
 import joblib
 
-vectorizer = joblib.load(pkg_resources.resource_filename('profanity_check', 'data/vectorizer.joblib'))
-model = joblib.load(pkg_resources.resource_filename('profanity_check', 'data/model.joblib'))
+vectorizer = joblib.load(
+    pkg_resources.resource_filename("profanity_check", "data/vectorizer2.joblib")
+)
+model = joblib.load(
+    pkg_resources.resource_filename("profanity_check", "data/model2.joblib")
+)
+
 
 def _get_profane_prob(prob):
-  return prob[1]
+    return prob[1]
+
 
 def predict(texts):
-  return model.predict(vectorizer.transform(texts))
+    return model.predict(vectorizer.transform(texts))
+
 
 def predict_prob(texts):
-  return np.apply_along_axis(_get_profane_prob, 1, model.predict_proba(vectorizer.transform(texts)))
+    return np.apply_along_axis(
+        _get_profane_prob, 1, model.predict_proba(vectorizer.transform(texts))
+    )
